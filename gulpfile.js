29479cba7f695f12ddc62643ab5b7a8da9511385
Yan Naing <tannaing@gmail.com>
2019-01-03 15:47:10 +0630

    Resolve merge conflic #2

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,8 @@ var livereload = require('gulp-livereload');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var zip = require('gulp-zip');
-<<<<<<< HEAD
-=======
 var uglify = require('gulp-uglify');
 var filter = require('gulp-filter');
->>>>>>> 3388283f02d99bf29a6a691a6c771960b884a901
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -29,19 +26,12 @@ var nodemonServerInit = function () {
     livereload.listen(1234);
 };
 
-<<<<<<< HEAD
-gulp.task('build', ['css'], function (/* cb */) {
-    return nodemonServerInit();
-});
-
-=======
 gulp.task('build', ['css', 'js'], function (/* cb */) {
     return nodemonServerInit();
 });
 
 gulp.task('generate', ['css', 'js']);
 
->>>>>>> 3388283f02d99bf29a6a691a6c771960b884a901
 gulp.task('css', function () {
     var processors = [
         easyimport,
@@ -60,8 +50,6 @@ gulp.task('css', function () {
         .pipe(livereload());
 });
 
-<<<<<<< HEAD
-=======
 gulp.task('js', function () {
     var jsFilter = filter(['**/*.js'], {restore: true});
 
@@ -76,16 +64,11 @@ gulp.task('js', function () {
         .pipe(livereload());
 });
 
->>>>>>> 3388283f02d99bf29a6a691a6c771960b884a901
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['css']);
 });
 
-<<<<<<< HEAD
-gulp.task('zip', ['css'], function () {
-=======
 gulp.task('zip', ['css', 'js'], function () {
->>>>>>> 3388283f02d99bf29a6a691a6c771960b884a901
     var targetDir = 'dist/';
     var themeName = require('./package.json').name;
     var filename = themeName + '.zip';
